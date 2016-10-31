7c9b7d5efe9311d295efcc3037dfeb66e8a28af3
Iggi <iggirex@gmail.com>
2016-10-30 23:37:51 -0600

    init commit

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var path = require("path");
+var body-parser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
+
+var DISHES_COLLECTION = "dishes";
+
+var app = express();
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
+var db;
+console.log("this is connection url: ", process.env.MONGODB_URI);
+
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  db = database;
+  console.log("Database connection ready");
+
+  var server = app.listen(process.env.PORT || 8080, function() {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
