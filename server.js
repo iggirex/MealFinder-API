983d8400e465b725fbe93b395d93dd6c61776ad2
Iggi <iggirex@gmail.com>
2016-11-01 12:25:40 -0600

    query syntax working for single item, testing multiple

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.get("/dishes", function(req, res) {
 app.get("/postDishes/:restrictions", function(req, res, next) {
   // res.send(req.params.restrictions)
   var filteredResult = db.collection(DISHES_COLLECTION)
-    .find({ tags: {$all: ["paleo"]} }).toArray(function(err, docs) {
+    .find({ tags: {$all: ["paleo", "vegan"]} }).toArray(function(err, docs) {
     res.status(200).json(docs);
   })
 })
